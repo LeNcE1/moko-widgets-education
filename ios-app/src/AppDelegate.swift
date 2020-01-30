0f8b4182c097539d420824d56ef5bf9a11deb43c
aperskevich <aperskevich@icerockdev.com>
2020-01-30 19:48:47 +0700

    MOKO Widgets #3 - styles

--- ios-app/src/AppDelegate.swift
+++ ios-app/src/AppDelegate.swift
@@ -16,10 +16,9 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
         let screen = app.rootScreen.instantiate()
         let rootViewController = screen.viewController
-
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = rootViewController
-        window?.makeKeyAndVisible()
+        window?.tintColor = UIColor(red:1.00, green:0.74, blue:0.27, alpha:1.0)
 
         return true
     }
